e63384e541cd359f53237967784b13a79948e24a
knowzek <71522616+knowzek@users.noreply.github.com>
2025-07-23 04:32:12 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+def get_library_config(library):
+    return {
+        "spreadsheet_name": "Facebook Events",
+        "worksheet_name": "Raw Events",
+        "log_worksheet_name": "Log",
+        "organizer_name": "Virginia Beach Parks & Recreation",
+        "email_recipient": "knowzek@gmail.com",
+        "event_name_suffix": " (Virginia Beach)"
+    }
